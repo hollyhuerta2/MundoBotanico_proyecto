9a3fc69e63e85fc105870e390ddfe4294ffffb71
Holly Huerta <100244790+hollyhuerta2@users.noreply.github.com>
2023-05-20 23:34:01 -0600

    Base de datos, llenado y tablas
    estos archivos aún sufrirán cambios

---
+++ LLENADO_MB.sql
@@ -0,0 +1,117 @@
+--llenado de tablas MUNDO BOTANICO
+
+--llenado de tabla FAMILIA de plantas
+insert into FAMILIA values
+	(1, 'ANACARDIACEAS','Las plantas de la familia Anacardiaceae se caracterizan por tener hojas alternas y compuestas, que a menudo tienen un alto contenido de aceites esenciales. También suelen tener flores pequeñas, generalmente de color blanco o verde amarillento, que se agrupan en inflorescencias llamadas panículas.'),
+	(2, 'BORAGINÁCEAS','Las plantas de la familia Boraginaceae se caracterizan por sus flores en forma de embudo o campana, que pueden ser de colores brillantes como el azul, el morado o el rosa. También tienen hojas alternas y generalmente ásperas, con pelillos o espinas que les dan una textura áspera al tacto.'),
+	(3, 'CACTÁCEAS','Las cactáceas se caracterizan por su capacidad para almacenar agua en sus tallos y hojas, lo que les permite sobrevivir en condiciones de sequía y aridez. También se distinguen por su forma única y variada, que puede incluir columnas, esferas, cilindros y muchas otras formas.'),
+	(4, 'CAMPANULÁCEAS','Las campanuláceas se caracterizan por sus flores en forma de campana o estrella, que pueden ser de colores variados, desde el blanco y el amarillo hasta el morado y el azul. También tienen hojas simples y alternas, y muchas especies tienen una savia lechosa.'),
+	(5, 'CARIOFOLIÁCEAS','Las cariofoliáceas se caracterizan por sus flores de cinco pétalos unidos en forma de embudo o de estrella, que pueden ser de colores variados, como el blanco, el rosa y el rojo. También tienen hojas simples, opuestas o en verticilos, y un tallo leñoso o herbáceo.'),
+	(6, 'CISTÁCEAS','Las cistáceas se caracterizan por sus flores grandes y vistosas, que tienen cinco pétalos y numerosos estambres. Las flores suelen ser de color blanco, rosa o amarillo y aparecen en la primavera y el verano. También tienen hojas pequeñas y suaves que suelen ser de color verde grisáceo.'),
+	(7, 'COMPUESTAS(ASTERÁCEAS)','Las compuestas se caracterizan por sus flores que están agrupadas en cabezuelas rodeadas por brácteas, lo que les da la apariencia de una sola flor grande. Cada cabezuela está compuesta por muchas flores individuales, que pueden ser tubulares o liguladas, y que pueden ser de diversos colores como blanco, amarillo, naranja, rojo y púrpura. Las hojas suelen ser simples, alternas y pueden ser lisas o dentadas.'),
+	(8, 'CONVOLVULÁCEAS','Las convolvuláceas se caracterizan por sus flores en forma de trompeta, que pueden ser blancas, rosadas, púrpuras o azules. Las hojas pueden ser simples o lobuladas y pueden ser de diferentes formas y tamaños según la especie. Las enredaderas de esta familia tienen un tallo largo y delgado que les permite trepar y crecer enredándose en otras plantas o estructuras.'),
+	(9, 'CRUCÍFERAS(BRASICÁCEAS)','Las crucíferas se caracterizan por sus flores con cuatro pétalos dispuestos en forma de cruz, de ahí el nombre de la familia. Las hojas suelen ser alternas y pueden ser lisas o dentadas. Muchas especies de crucíferas tienen raíces pivotantes que les permiten extraer nutrientes del suelo de manera eficiente.'),
+	(10, 'CUCURBITÁCEAS','Las cucurbitáceas se caracterizan por sus frutos carnosos y generalmente grandes, que se llaman pepones y tienen semillas en su interior. Las flores son generalmente grandes y de color amarillo brillante. Las hojas pueden ser simples o lobuladas y pueden ser de diferentes formas y tamaños según la especie.'),
+	(11, 'DIPSACÁCEAS','Las dipsacáceas se caracterizan por sus cabezas de flores globulares o cilíndricas, que consisten en muchas flores individuales, cada una con cuatro pétalos fusionados. Las hojas suelen ser opuestas y pueden ser lisas o dentadas. Muchas especies de dipsacáceas tienen tallos rectos y robustos que les permiten crecer en condiciones de viento y mantenerse erguidas.'),
+	(12, 'ERICÁCEAS','Las ericáceas se caracterizan por sus flores en forma de campana o embudo, que generalmente tienen cuatro o cinco pétalos fusionados. Las hojas suelen ser pequeñas, duras y coriáceas, y algunas especies tienen hojas perennes que les permiten resistir los inviernos fríos. Muchas especies de ericáceas tienen raíces superficiales y una relación simbiótica con hongos micorrízicos que les permiten obtener nutrientes y agua de suelos pobres en nutrientes.'),
+	(13, 'ESCROFULARIÁCEAS','Plantas generalmente herbáceas y más raramente leñosas (Veronica fruticans, V. fruticulesa). Son de desarrollo plurianual o más raramente anual. Tienen las hojas simples, alternas o raramente opuestas (Verbascum, Scrophularia). Las flores constan de cinco pétalos soldados en un largo tubo o raramente de cuatro piezas soldadas en un corto tubo (Verónica).'),
+	(14, 'GENCIANÁCEAS','Las Gentianáceas son una familia de plantas que se caracterizan por tener flores de colores brillantes y hojas opuestas.Algunas especies de Gentianáceas se utilizan en la medicina tradicional para tratar afecciones digestivas, hepáticas y respiratorias. '),
+	(15, 'GERANIÁCEAS','Las Geraniáceas son una familia de plantas que incluye más de 800 especies diferentes en todo el mundo. Se caracterizan por tener flores con cinco pétalos y hojas redondeadas y dentadas. Algunas especies son muy populares en jardinería debido a sus flores vistosas y su fácil cultivo, como los geranios y las pelargonias.'),
+	(16, 'HIPERICÁCEAS','Las Hipericáceas son una familia de plantas que incluye alrededor de 600 especies diferentes en todo el mundo. Se caracterizan por tener flores con cinco pétalos amarillos y numerosos estambres.Además, algunas especies de Hipericáceas se utilizan en la medicina tradicional para tratar la ansiedad, la depresión y otras afecciones relacionadas con el sistema nervioso. También se ha investigado su potencial en la prevención y tratamiento del cáncer.'),
+	(17, 'IRIDÁCEAS','Las Iridáceas son una familia de plantas que incluye más de 2,000 especies diferentes en todo el mundo. Se caracterizan por tener flores grandes y vistosas con seis pétalos y un tallo largo y delgado que sostiene la flor.'),
+	(18, 'LABIADAS','Las Labiadas, también conocidas como Lamiáceas, son una familia de plantas que incluye alrededor de 7,000 especies diferentes en todo el mundo. Se caracterizan por tener flores con forma de labio, con dos pétalos superiores fusionados y tres inferiores separados. Las hojas suelen ser aromáticas y se disponen en pares opuestos. '),
+	(19, 'LEMNÁCEAS','Las Lemnáceas, también conocidas como lentejas de agua, son una familia de plantas acuáticas pequeñas y flotantes. Se caracterizan por tener hojas muy reducidas que forman una especie de disco flotante, sin tallo ni raíces verdaderas. Algunas especies son muy populares en acuarios y estanques debido a su capacidad para filtrar el agua y reducir la proliferación de algas. '),
+	(20, 'LILIÁCEAS','Las Liliáceas son una familia de plantas que incluye alrededor de 3,000 especies diferentes en todo el mundo. Se caracterizan por tener flores grandes y vistosas con seis pétalos y un bulbo subterráneo que almacena nutrientes. Las hojas suelen ser largas y estrechas y se disponen en forma de roseta basal'),
+	(21, 'MALVÁCEAS','Las Malváceas son una familia de plantas que incluye alrededor de 4,000 especies diferentes en todo el mundo. Se caracterizan por tener flores grandes y vistosas con cinco pétalos que se disponen en forma de copa y un fruto que se divide en segmentos cuando madura. '),
+	(22, 'ONAGRÁCEAS','Las Onagráceas son una familia de plantas que incluye alrededor de 650 especies diferentes en todo el mundo. Se caracterizan por tener flores grandes y vistosas con cuatro pétalos y un fruto seco que se abre en cuatro partes. Las hojas pueden ser simples o compuestas y se disponen de manera alterna en el tallo. '),
+	(23, 'OROBANCÁCEAS','Se caracterizan por ser plantas parásitas que no tienen clorofila, lo que significa que no pueden producir su propio alimento a través de la fotosíntesis. En su lugar, las Orobanquéceas extraen nutrientes de las raíces de otras plantas a través de unas estructuras especializadas llamadas haustorios.'),
+	(24, 'ORQUIDÁCEAS','Las orquídeas son conocidas por su belleza y su valor en la industria florística, y son una de las plantas más populares en jardinería. Además, algunas especies de Orquídeas se utilizan en la medicina tradicional para tratar diversas afecciones, como la ansiedad y las enfermedades respiratorias. '),
+	(25, 'PAPILIONÁCEAS','Se caracterizan por tener flores grandes y vistosas que se disponen en forma de mariposa, con una flor grande y vistosa en la parte superior y dos flores más pequeñas en la parte inferior. Las Papilionáceas tienen frutos que suelen ser legumbres, como las habas, los guisantes y los frijoles, que se dividen en dos partes cuando maduran.'),
+	(26, 'PAPAVERÁCEAS','Se caracterizan por tener flores grandes y vistosas, de colores brillantes, que se disponen en forma solitaria en la parte superior del tallo. Las hojas suelen ser simples y lobuladas y se disponen de manera alterna en el tallo. Muchas especies de Papaveráceas producen látex, un líquido lechoso y pegajoso que se utiliza en la fabricación de opio y otros medicamentos. '),
+	(27, 'PINÁCEAS','Se caracterizan por tener hojas aciculares o escamosas que se disponen en forma de espiral en el tallo. Las Pináceas son importantes productoras de madera y se utilizan para la construcción, la fabricación de papel, la producción de muebles y otros productos de madera. '),
+	(28, 'POLEMONIÁCEAS','Se caracterizan por tener flores hermosas y vistosas, que pueden ser de varios colores y formas, y se disponen en grupos o solitarias en el tallo. Las hojas pueden ser simples o compuestas y se disponen de manera alternada en el tallo. '),
+	(29, 'POTAMOGETONÁCEAS','Las Potamogetonáceas son una familia de plantas acuáticas que se encuentran en todo el mundo, en aguas dulces y salobres. Se caracterizan por tener hojas sumergidas, alargadas y lanceoladas, y por florecer bajo el agua. Las flores son pequeñas, verdosas y se disponen en espigas o racimos.'),
+	(30, 'PRIMULÁCEAS','Se caracterizan por tener hojas simples y enteras, que pueden ser alargadas o redondeadas, y por producir flores hermosas y vistosas en primavera y verano.'),
+	(31, 'QUENOPODIÁCEAS','Se caracterizan por tener hojas simples y a menudo carnosas, y por producir flores pequeñas y poco vistosas en grupos o solitarias en el tallo. Las Quenopodiáceas son adaptadas a ambientes desérticos y salinos, y son importantes para la alimentación humana y animal.'),
+	(32, 'RANUNCULÁCEAS','Se caracterizan por tener hojas alternas, simples o divididas en varios lóbulos, y por producir flores hermosas y vistosas con muchos pétalos en diversos colores, como el amarillo, el blanco y el rojo.'),
+	(33, 'ROSÁCEAS','Las Rosáceas son una familia de plantas angiospermas que incluyen más de 3,000 especies, incluyendo árboles frutales como manzanos, perales, duraznos, cerezos, ciruelos y albaricoques, así como arbustos como las rosas, fresas y frambuesas. Las Rosáceas se caracterizan por tener flores con cinco pétalos, un cáliz con cinco sépalos y un conjunto de estambres y pistilos.'),
+	(34, 'RUBIÁCEAS','Algunos miembros notables de esta familia incluyen el café, el quina y la ipecacuana. Las Rubiáceas se caracterizan por tener hojas simples, opuestas y sin estípulas, así como por tener flores que generalmente tienen cinco pétalos y cinco sépalos. '),
+	(35, 'SAXIFRAGÁCEAS','Las Saxifragáceas se caracterizan por tener hojas generalmente dentadas o lobuladas, así como por tener flores con cinco pétalos y cinco sépalos. Muchas especies de Saxifragáceas también tienen un crecimiento rastrero y forman densas matas en la roca o en el suelo, lo que las hace populares en jardinería. '),
+	(36, 'SOLANÁCEAS','Las Solanáceas se caracterizan por tener flores con cinco pétalos unidos en forma de estrella, un cáliz con cinco sépalos y una corola con forma de campana.'),
+	(37, 'UMBELÍFERAS','Las Umbelíferas se caracterizan por tener flores pequeñas y reunidas en umbelas, que son estructuras en forma de sombrilla sostenidas por rayos. Además, sus hojas suelen ser finamente divididas y muy parecidas al encaje. Muchas especies de Umbelíferas también tienen frutos secos y alargados, que se dividen en dos partes cuando maduran. ');
+
+--llenado de tabla ZONA
+insert into ZONA values (1,'Zonas Templadas','Europa, Asia, Norteamérica'),
+	(2,'Zonas Tropicales','América Central y del Sur, el sur de Asia y África.'),
+	(3,'Zonas Áridas','desiertos del Sahara, Atacama y Mojave.'),
+	(4,'Zonas Frías','las regiones polares y las zonas montañosas de alta altitud.');
+
+--llenado de tabla TIPOPLANTA
+insert into TIPOPLANTA values (1,'Terrestres','crecen en la tierra firme, incluyendo bosques, praderas, sabanas, desiertos, tundras y pantanos.'),
+	(2,'Acuáticas','crecen en el agua, incluyendo algas, plantas sumergidas, plantas flotantes y plantas emergentes.'),
+	(3,'Epífitas','crecen sobre otras plantas, sin tener raíces en el suelo.'),
+	(4,'Parasitarias','son plantas que obtienen nutrientes de otras plantas.');
+
+
+--llenado de tabla de TIPO HOJA
+insert into TIPOHOJA values (1,'Hojas simples','una sola hoja en el tallo'),
+	(2,'Hojas compuestas','múltiples hojas en el tallo'),
+	(3,'Hojas dentadas','son hojas con bordes irregulares con pequeñas protuberancias en forma de dientes.'),
+	(4,'Hojas lobuladas','son hojas que tienen hendiduras profundas en los bordes'),
+	(5,'Hojas aciculares','hojas largas y delgadas en forma de aguja, típicas de plantas como los pinos y las coníferas.'),
+	(6,'Hojas escamosas','hojas cortas y anchas que se superponen entre sí'),
+	(7,'Hojas flotantes','Estas hojas suelen tener una gran superficie para captar la luz solar y pueden estar diseñadas para resistir el daño causado por el agua.'),
+	(8,'Hojas sumergidas','Estas hojas suelen ser largas y delgadas, y pueden tener una estructura similar a las hojas aciculares de las plantas terrestres.'),
+	(9,'Hojas emergentes','Son hojas que crecen bajo el agua, pero se extienden por encima de la superficie.'),
+	(10,'Hojas anchas','algunas plantas acuáticas pueden tener hojas grandes y anchas que se asemejan a las hojas de plantas terrestres'),
+	(11,'Hojas gruesas','pueden ser gruesas y carnosas para ayudar a retener la humedad y resistir la deshidratación en ambientes secos.'),
+	(12,'Hojas pequeñas','las hojas pequeñas pueden crecer más densamente en el tallo para aumentar la capacidad de fotosíntesis.'),
+	(13,'Hojas colgantes','son colgantes para facilitar la captación del agua de lluvia y la humedad'),
+	(14,'Hojas reducidas','estas hojas no necesitan captar la luz para realizar la fotosíntesis'),
+	(15,'Hojas modificadas en escamas','Estas hojas están altamente modificadas y no realizan fotosíntesis, pero ayudan a retener la humedad y a proteger el tallo de la planta parasitaria.'),
+	(16,'Hojas mixtas','pueden ser hojas que no realicen la fotosíntesis, pero captar los nutrientes');
+
+--llenado de tabla CUIDADO de plantas
+insert into CUIDADOPLAN values (1, 1,'La mayoría de las plantas terrestres requieren un riego regular para mantenerse saludables. Es importante no excederse en el riego, ya que el exceso de agua puede asfixiar las raíces y causar pudrición. También es importante asegurarse de que el agua llegue a las raíces, no solo a la superficie del suelo. Las plantas terrestres necesitan luz para realizar la fotosíntesis, pero la cantidad y el tipo de luz que necesitan varían según la especie'),
+	(2, 2,'Las plantas acuáticas requieren agua limpia y bien oxigenada para sobrevivir. Es importante cambiar el agua regularmente y asegurarse de que esté libre de contaminantes. También es importante asegurarse de que haya una buena circulación de agua para mantener niveles adecuados de oxígeno.'),
+	(3, 3,'Las plantas epífitas no crecen en tierra, por lo que es importante proporcionarles un sustrato adecuado para que puedan fijarse. Algunas opciones pueden incluir musgo sphagnum, corteza de árbol o fibra de coco.'),
+	(4, 4,'Las plantas parasitarias requieren un huésped para obtener nutrientes, por lo que es importante asegurarse de que se encuentren en una ubicación adecuada para su especie y que tengan acceso a un huésped. requieren luz brillante, pero las necesidades de luz pueden variar según la especie. Las plantas parasitarias pueden obtener agua del huésped, pero también pueden necesitar agua adicional si se cultivan en macetas o en un ambiente artificial.');
+
+--llenado de tabla USUARIOS 
+insert into USUARIO values(1,'Holly','Huerta','holly.cg@outlook.com'),
+	(2,'Jesús','López','jesusLR@outlook.com'),
+	(3,'Mariana','Prado','mary50@hotmail.com'),
+	(4,'Jennifer','Cruz','jennyCC@outlook.com');
+	
+insert into FAMILIA values(38,'Bromeliaceae','Esta familia incluye muchas especies de plantas tropicales y subtropicales');
+insert into FAMILIA values(39,' Nymphaeaceae','Esta familia incluye muchas especies de plantas acuáticas ');
+
+
+--llenado de tabla PLANTAS 
+insert into PLANTAS values (1,'mangos','Mangifera indica',1,1,2,1),
+	(2,'Rosas','Rosa spp',1,2,1,33),
+	(3,'Girasoles',' Helianthus annuus',1,10,2,2),
+	(4,'Margaritas','Leucanthemum vulgare',1,1,1,7),
+	(5,'Tulipanes','Tulipa spp',1,5,1,20),
+	(6,'Claveles','Dianthus caryophyllus',1,4,1,5),
+	(7,'Cactus de barril','Echinocactus grusonii',1,16,2,3),
+	(8,'Aloe vera','Sábila',1,11,2,20),
+	(9,'Jade','Crassula ovata',1,11,1,3),
+	(10,'Lirios','Lilium spp',1,1,2,20),
+	(11,'Lirios de agua','Zantedeschia aethiopica',2,8,1,39),
+	(12,'Bromelias','Bromeliáceas',3,2,2,38),
+	(13,'Orquídeas','Orchidaceae',3,1,1,24),
+	(14,'Hiedra terrestre','Hedera helix',4,13,1,18),
+	(15,'Petunias','Petunia Hybrida',1,2,2,36);
+	
+--llenado tabla plantas usuarios
+insert into PLANTA_US values (1,1,5),(2,2,9),(3,3,8),(4,4,3);
+
+
+
+
+
+
+
+
+
