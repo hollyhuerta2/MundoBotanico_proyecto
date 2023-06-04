1b6d0ef40e89f2cf9100860eb7f164823a3e8497
Holly <holly.cg@outlook.com>
2023-06-04 14:41:36 -0600

    New commit! cambios nuevos

--- src/main/java/Controlador/SvUsuarios.java
+++ src/main/java/Controlador/SvUsuarios.java
@@ -1,19 +1,28 @@
 package Controlador;
 
+import Datos.UsuarioDAO;
+import modelo.Usuario;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.*;
 
 @WebServlet(name = "SvUsuarios", value = "/SvUsuarios")
 public class SvUsuarios extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Usuario> usuario = new UsuarioDAO().selecionar();
+        System.out.println("Usuarios"+usuario);
+        request.setAttribute("usuarios",usuario);
+        request.getRequestDispatcher("Usuario.jsp").forward(request,response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
     }
 }
---
+++ src/main/webapp/WEB-INF/Usuario.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: holly
+  Date: 04/06/2023
+  Time: 03:38 p. m.
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import ="java.io.*, java.util.*"%>
+<html>
+<head>
+    <title>Usuario JSP</title>
+</head>
+<body>
+    <h2>Prueba jsp</h2>
+
+</body>
+</html>
